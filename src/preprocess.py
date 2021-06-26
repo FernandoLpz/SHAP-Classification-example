9ab16c73845cbf41d0dbcb531178dffeeecb6517
FernandoLpz <fer.neutron@gmail.com>
2021-06-26 16:16:04 -0500

    Add data transformation

---
+++ src/preprocess.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+class Data:
+
+    def __init__(self, csv_path: str) -> None:
+        self.csv_path = csv_path
+        self.data = pd.read_csv(self.csv_path)
+
+        self._transform()
+        self._split()
+
+        pass
+
+    def _transform(self) -> None:
+        self.data.drop(['id'], axis=1, inplace=True)
+        self.data.columns = self.data.columns.str.replace(' ', '_')
+
+        self.classes = {0: 'M', 1: 'B'}
+        self.data['diagnosis_result'].replace({'M': 0, 'B': 1}, inplace=True)
+
+    def _split(self) -> None:
+        self.x_train, self.x_test, self.y_train, self.y_test = \
+            train_test_split(self.data.drop(['diagnosis_result'], axis=1), self.data['diagnosis_result'], test_size=0.15)
+
+    def __len__(self) -> int:
+        return self.data.shape[0]
+
+    def __str__(self) -> str:
+        return f"Dataset with {self.data.shape[0]} samples and {self.data.shape[1] - 1} features"
+
+    def __repr__(self) -> str:
+        return f"Data({self.csv_path})"
